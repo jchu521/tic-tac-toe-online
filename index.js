7552c27a12caf7159fb77aa725e9102af33eef26
Jonathan Chueh <jonathanchueh1992@gmail.com>
2019-12-20 19:07:40 +1100

    add socket.io

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
+const socketIO = require("socket.io");
+
+const io = socketIO(server);
 
 const app = express();
+const server = http.createServer(app);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -13,4 +17,71 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
+
+// This is what the socket.io syntax is like, we will work this later
+io.on("connection", socket => {
+  console.log("User connected");
+
+  /**
+   * Create a new game room and notify the creator of game.
+   */
+  socket.on("createGame", function(data) {
+    socket.join(data.roomName);
+    socket.emit("player1", { message: "waiting for player join..." });
+  });
+
+  /**
+   * Connect the Player 2 to the room he requested. Show error if room full.
+   */
+  socket.on("joinGame", function(data) {
+    //check room exist
+    var room = io.nsps["/"].adapter.rooms[data.roomName];
+    //Only one player in room
+    if (room && room.length == 1) {
+      socket.join(data.roomName);
+      socket.broadcast.to(data.roomName).emit("player1", { joined: true });
+      socket.emit("player2");
+    } else {
+      socket.emit("err", {
+        message: "Sorry, The room is full!"
+      });
+    }
+  });
+
+  /**
+   * Handle the turn played by either player and notify the other.
+   */
+  socket.on("playTurn", function(data) {
+    if (data.mark === "O") {
+      socket.broadcast.to(data.roomName).emit("player1 turn", {
+        message: "waiting "
+      });
+      socket.broadcast.to(data.roomName).emit("player2 turn", {
+        message: "Your Turn"
+      });
+    } else {
+      socket.broadcast.to(data.roomName).emit("player1 turn", {
+        message: "Your Turn"
+      });
+      socket.broadcast.to(data.roomName).emit("player2 turn", {
+        message: "waiting "
+      });
+    }
+
+    socket.broadcast.to(data.roomName).emit("turnPlayed", {
+      newBoard: data.newBoard,
+      mark: data.mark
+    });
+  });
+
+  socket.on("play again", data => {
+    socket.emit("restart", { board: data.board });
+  });
+
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
